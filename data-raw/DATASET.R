f3209a63d20045d771dc3d37a8c54df30fadffd3
Meng Liu <veronica.lm@live.cn>
2022-02-09 10:56:50 +0000

    add data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,17 @@
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
+
+library(gutenbergr)
+pacman::p_load(tidyverse,rio)
+
+id <- gutenberg_metadata %>% 
+      filter(str_detect(title,"The Moon and Sixpence|The Age of Innocence|The Cask"), 
+             has_text==T) %>% 
+      pull(gutenberg_id)
+
+books <- gutenberg_download(id,
+         meta_fields = c("title"))
+
+usethis::use_data(books,overwrite=T)
+
